a98973a73b0fb0978506b765876feb4c4f2ed7d3
Tim <harvard90873@gmail.com>
2021-08-22 18:07:55 +0200

    update class fields' types

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,7 @@
-import { Cell } from "./ui/App";
-
 export class ListNode {
-  value: Cell;
+  value: number;
   next: ListNode | undefined;
-  constructor(value: Cell, next?: ListNode) {
+  constructor(value: number, next?: ListNode) {
     this.value = value;
     this.next = next;
   }
@@ -13,7 +11,7 @@ export class LinkedList {
   head: ListNode;
   tail: ListNode;
 
-  constructor(value: Cell) {
+  constructor(value: number) {
     const node = new ListNode(value);
     this.head = node;
     this.tail = node;
