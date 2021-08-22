a203a5d52178dfd1bbefd7865ca679efeb599926
Tim <harvard90873@gmail.com>
2021-08-22 14:11:31 +0200

    register key listeners

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import {Grid} from "./Grid";
+import { useEffect } from "react";
+import { Grid } from "./Grid";
 
-export const App = () =>  {
+export const App = () => {
+  // register keys
+  useEffect(() => {
+    const keyDownHandler = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "ArrowLeft": {
+          break;
+        }
+        case "ArrowRight": {
+          break;
+        }
+        case "ArrowDown": {
+          break;
+        }
+        case "ArrowUp": {
+          break;
+        }
+      }
+    };
+    window.addEventListener("keydown", keyDownHandler);
+
+    return () => {
+      window.removeEventListener("keydown", keyDownHandler);
+    };
+  });
 
   return (
-    <div className="App">
-      <Grid />
-    </div>
+    <>
+      <Grid dimensions={{ rows: 30, cols: 100 }} />
+    </>
   );
-}
-
+};
