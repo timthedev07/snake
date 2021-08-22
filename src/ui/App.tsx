899a87a071fe2f8abe6b734e736ae71495e24ca6
Tim <harvard90873@gmail.com>
2021-08-22 22:42:30 +0200

    complete automatic snake movement

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useMemo, useState } from "react";
-import { LinkedList, ListNode, NodeValue } from "src/lib/classes";
+import { LinkedList, ListNode } from "src/lib/classes";
 import {
   createGrid,
   getInitialSnakeValue,
   getNewNodeCoords,
-  getOppositeDirection,
+  getNextCoordsInDirection,
   isOutOfBounds,
   randIntInRange,
+  useInterval,
 } from "src/lib/utils";
 
 export enum Direction {
@@ -26,10 +27,15 @@ export const App = () => {
     new Set([initial.cell])
   );
   const [snake, setSnake] = useState<LinkedList>(new LinkedList(initial));
-  const [foodCell, setFoodCell] = useState<number>(
-    randIntInRange(0, SIZE ** 2 - 1)
-  );
+  const [foodCell, setFoodCell] = useState<number>(snake.head.value.cell + 3);
   const [score, setScore] = useState<number>(0);
+  const [lost, setLost] = useState<boolean>(false);
+
+  console.log(foodCell);
+
+  useInterval(() => {
+    handleSnakeMovement();
+  }, 150);
 
   // register keys
   useEffect(() => {
@@ -64,18 +70,74 @@ export const App = () => {
     };
   });
 
-  const handleFoodConsumption = () => {
+  const handleLose = () => {
+    // cry();
+    setLost(true);
+  };
+
+  const handleSnakeMovement = () => {
+    const newHeadCoords = getNextCoordsInDirection(snake.head.value, direction);
+
+    // if snake crashes into the border
+    if (isOutOfBounds(newHeadCoords[0], newHeadCoords[1])) {
+      handleLose();
+      return;
+    }
+
+    // if snake crashes into himself
+    // i.e. the new head crashes into an existing snake grid
+    const newHeadCell = grid[newHeadCoords[0]][newHeadCoords[1]];
+    if (snakeCells.has(newHeadCell)) {
+      handleLose();
+      return;
+    }
+
+    // create new head node
+    const newHead = new ListNode({
+      row: newHeadCoords[0],
+      col: newHeadCoords[1],
+      cell: newHeadCell,
+    });
+
+    // update head
+    //// store the current head(the rest of the snake is connected to it, this way we don't lose it)
+    const currentHead = snake.head;
+    //// set the head to the newly created head
+    snake.head = newHead;
+    //// connect the rest of the snake with the new head
+    currentHead.next = newHead;
+
+    // add new head to the set and remove the dangling tail
+    const newSnakeCells = new Set(snakeCells);
+    newSnakeCells.delete(snake.tail.value.cell);
+    newSnakeCells.add(newHeadCell);
+
+    // move the tail pointer to the current tail's next
+    snake.tail = snake.tail.next as any;
+    // prevent tail from getting detached
+    if (snake.tail === null) snake.tail = snake.head;
+
+    // if head is at food(aka, consumed the food)
+    if (newHeadCell === foodCell) {
+      handleSnakeGrowth(newSnakeCells);
+      handleFoodConsumption(newSnakeCells);
+    }
+
+    setSnakeCells(newSnakeCells);
+  };
+
+  const handleFoodConsumption = (newSnakeCells: Set<number>) => {
     const maxCount = SIZE * SIZE - 1;
     let nextFoodCell: number;
     while (true) {
       nextFoodCell = randIntInRange(0, maxCount);
-      if (!snakeCells.has(nextFoodCell) && foodCell !== nextFoodCell) break;
+      if (!newSnakeCells.has(nextFoodCell) && foodCell !== nextFoodCell) break;
     }
     setFoodCell(nextFoodCell);
     setScore((prev) => prev + 1);
   };
 
-  const handleSnakeGrowth = () => {
+  const handleSnakeGrowth = (newSnakeCells: Set<number>) => {
     const newNodeCoords = getNewNodeCoords(snake.tail, direction);
     // can't grow
     if (isOutOfBounds(newNodeCoords[0], newNodeCoords[1])) {
@@ -91,7 +153,7 @@ export const App = () => {
     newTail.next = currTail;
     snake.tail = newTail;
 
-    const updatedCells = new Set(snakeCells);
+    const updatedCells = new Set(newSnakeCells);
 
     updatedCells.add(newTailCell);
 
@@ -100,19 +162,18 @@ export const App = () => {
 
   return (
     <div>
-      <button
-        onClick={() => {
-          handleSnakeGrowth();
-        }}
-      >
-        Grow
-      </button>
       {grid.map((row, i) => (
         <div key={i} className="row">
           {row.map((cell, j) => (
             <div
               key={`${i}-${j}`}
-              className={`cell ${snakeCells.has(cell) ? "snake" : "empty"}`}
+              className={`cell ${
+                snakeCells.has(cell)
+                  ? "snake"
+                  : foodCell === cell
+                  ? "food"
+                  : "empty"
+              }`}
             ></div>
           ))}
         </div>
