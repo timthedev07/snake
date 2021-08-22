15664db0478b9952eb17e3e08ea6bd2baa4e1357
Tim <harvard90873@gmail.com>
2021-08-22 14:33:19 +0200

    set up grid

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+type Cell = "empty" | "food" | "snake";
+type GameStatus = "lost" | "playing" | "won";
+
+const SIZE = 20;
 
 export const App = () => {
+  const [status, setStatus] = useState<GameStatus>("playing");
+  const [grids, setGrids] = useState<Cell[][]>(
+    [...Array(SIZE)].map(() => [...Array(SIZE)].map(() => "empty"))
+  );
+
   // register keys
   useEffect(() => {
     const keyDownHandler = (e: KeyboardEvent) => {
@@ -26,5 +36,15 @@ export const App = () => {
     };
   });
 
-  return <></>;
+  return (
+    <div>
+      {grids.map((row) => (
+        <div className="row">
+          {row.map((cell) => (
+            <div className={`cell ${cell}`}></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
